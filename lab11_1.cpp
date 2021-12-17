3e6e2b57ce74e45d98404c1f98c0a803e7c957e2
sani <sanit_ak@cmu.ac.th>
2021-12-17 17:34:09 +0700

    เสร็จ

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,27 +1,51 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
+char grade;
+	int i=1,A,B,C,D,F;
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
+	int count[5] = {A=0,B=0,C=0,D=0,F=0};
+	cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
 	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
-	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
+		cout << "Student [" << i << "]: ";
+		cin >> grade ;
+		if (grade == '0')
+		break;
+
+		if(grade == 'A'){
+			count[0]+=1;
+			i++;
+			}else{
+				if(grade == 'B'){
+					count[1]+=1; 
+					i++;
+				}else{ 
+						if(grade == 'C'){
+							count[2]+=1;
+							i++;
+						}else{ 
+								if(grade == 'D'){
+									count[3]+=1;
+									i++;
+								}else{
+										if(grade == 'F'){
+											count[4]+=1;
+											i++;
+										}else{
+												cout<<"Wrong input. Please input again.\n";
+											}
+								}
+						}
+				}
+			}	
+	}while(1);
+	cout << "In total "<< i-1 <<" students.\n";
+	cout << "A = " <<count[0]<<", ";
+	cout << "B = " <<count[1]<<", ";
+	cout << "C = " <<count[2]<<", ";
+	cout << "D = " <<count[3]<<", ";
+	cout << "F = " <<count[4];	
+
 	return 0;
-}
+};
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,45 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+float calculateSD(float data[],float k) {
+  float s = 0.0, mean, standardDeviation = 0.0;
+  int j;
+
+  for(j = 0; j < k; ++j) {
+    s += data[j];
+  }
+
+  mean = s / k;
+
+  for(j = 0; j < k; ++j) {
+    standardDeviation += pow(data[j] - mean, 2);
+  }
+
+  return sqrt(standardDeviation / k);
+}
+
+int main(){
+    int i=0;
+    float sum=0,SD;
+    float data[83]={};
+    string textline ;
+    ifstream source("score.txt");
+    while (getline(source,textline))
+    {
+        data[i]= atof(textline.c_str());
+        sum += atof(textline.c_str());
+        i++;
+    }
+    SD=calculateSD(data,i);
+
+    cout << "Number of data = "<< i <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<<sum/i<<"\n";
+    cout << "Standard deviation = "<< SD<<"\n";
+}
